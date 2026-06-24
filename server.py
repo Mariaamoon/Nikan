07ab2b3b5a551922f8eb3bfe2d266ca95f81f837
Maria <mariaamoon008@gmail.com>
2026-06-24 23:02:01 +0330

    accept input

--- server.py
+++ server.py
@@ -14,3 +14,20 @@ def quote():
         "text":"Believe in yourself.",
         "author":"Unknown"
     }
+
+from pydantic import BaseModel
+
+
+class UserMessage(BaseModel):
+    text: str
+
+#from train import model, quote_embeddings
+
+@app.post("/recommend")
+def recommend(msg: UserMessage):
+
+    print(msg.text)
+
+    return {
+        "received": msg.text
+    }
