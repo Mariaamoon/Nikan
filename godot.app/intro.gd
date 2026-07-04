262c1c2458fceafc77872e4cbe46d9b7b3a09078
Maria <mariaamoon008@gmail.com>
2026-07-04 14:14:55 +0330

    not sure

--- godot.app/intro.gd
+++ godot.app/intro.gd
@@ -3,16 +3,30 @@ extends Node
 @onready var start_button = $startbutton
 @onready var name_input = $NameInput
 
+var pending_name = ""
+var server_is_ready = false
 
 func _ready():
-
-	pass
-
-func _on_name_saved():
-
-	get_tree().change_scene_to_file("res://Main.tscn")
-
+	Network.server_ready.connect(_on_server_ready)
+	Network.name_saved.connect(_on_name_saved)
+	Network.quote_received.connect(_on_quote_received)
 
 func _on_startbutton_pressed() -> void:
-	Network.send_name(name_input.text)
-	Network.name_saved.connect(_on_name_saved)
+	pending_name = name_input.text
+	if server_is_ready:
+		Network.send_name(pending_name)
+		get_tree().change_scene_to_file("res://Main.tscn")
+	else:
+		print("Waiting for server to be ready...")
+
+func _on_server_ready():
+	server_is_ready = true
+	if pending_name != "":
+		Network.send_name(pending_name)
+		get_tree().change_scene_to_file("res://Main.tscn")
+
+func _on_name_saved():
+	print("Name was saved!")
+
+func _on_quote_received(data):
+	print("Got quote:", data)
--- godot.app/network.gd
+++ godot.app/network.gd
@@ -4,116 +4,98 @@ signal server_ready
 signal name_saved
 signal quote_received(data)
 
-var health_http : HTTPRequest
-var api_http : HTTPRequest
+var health_http: HTTPRequest
+var name_http: HTTPRequest
+var quote_http: HTTPRequest
 
 var checking_server = false
 
 func _ready():
-
 	health_http = HTTPRequest.new()
 	add_child(health_http)
 	health_http.request_completed.connect(_on_health_completed)
 
-	api_http = HTTPRequest.new()
-	add_child(api_http)
-	api_http.request_completed.connect(_on_api_completed)
+	name_http = HTTPRequest.new()
+	add_child(name_http)
+	name_http.request_completed.connect(_on_name_completed)
+
+	quote_http = HTTPRequest.new()
+	add_child(quote_http)
+	quote_http.request_completed.connect(_on_quote_completed)
 
 	start_server()
 
 
 func start_server():
-
-	OS.create_process(
+	var pid = OS.create_process(
 	"C:/Users/jam/AppData/Local/Programs/Python/Python313/python.exe",
-		 ["-m",
-		"uvicorn",
-		"server:app",
-		"--host", "127.0.0.1",
-		"--port", "8000"]
-	)
-
-	await get_tree().create_timer(2).timeout
-
+	["-m", "uvicorn", "server:app", "--host", "127.0.0.1", "--port", "8000"]
+)
+	print("Server process PID:", pid)
+	await get_tree().create_timer(1).timeout
 	check_server()
 
 
 func check_server():
-
 	checking_server = true
-
-	health_http.request(
-        "http://127.0.0.1:8000/"
-	)
+	print("Checking server...")
+	health_http.request("http://127.0.0.1:8000/")
 
 
 func send_name(name):
-	print("Sending name:", name)
+	if checking_server == true:
+		print("Sending name:", name)
+		var body = {"name": name}
+		var headers = ["Content-Type: application/json"]
+		var err = name_http.request(
+			"http://127.0.0.1:8000/set_name",
+			headers,
+			HTTPClient.METHOD_POST,
+			JSON.stringify(body)
+		)
+		print("Request result:", err)
 
-	var body = {
-		"name": name
-	}
-	print("JSON:", JSON.stringify(body))
 
-	var headers = [
-        "Content-Type: application/json"
-	]
-
-	var err = api_http.request(
-		"http://127.0.0.1:8000/set_name",
+func request_quote(text):
+	print("Requesting quote:", text)
+	var body = {"text": text}
+	var headers = ["Content-Type: application/json"]
+	var err = quote_http.request(
+		"http://127.0.0.1:8000/recommend",
 		headers,
 		HTTPClient.METHOD_POST,
 		JSON.stringify(body)
 	)
 	print("Request result:", err)
 
-func request_quote(text):
-	print("Requesting quote:", text)
 
-	var body = {
-		"text": text
-	}
-
-	var headers = [
-        "Content-Type: application/json"
-	]
-
-	api_http.request(
-		"http://127.0.0.1:8000/recommend",
-		headers,
-		HTTPClient.METHOD_POST,
-		JSON.stringify(body)
-	)
-
-
-func _on_health_completed(result,response_code,headers,body):
-
+func _on_health_completed(result, response_code, headers, body):
+	print("Health check result:", result, " response code:", response_code)
 	if response_code != 200:
-
 		await get_tree().create_timer(1).timeout
-
 		check_server()
-
 		return
-
+	checking_server = false
 	emit_signal("server_ready")
 
 
-func _on_api_completed(result,response_code,headers,body):
-
+func _on_name_completed(result, response_code, headers, body):
 	var response = body.get_string_from_utf8()
-
-	print(response)
-
+	print("Result enum:", result)
+	print("Response code:", response_code)
+	print("Response code:", response_code)
+	print("Name response:", response)
 	var data = JSON.parse_string(response)
-	print("API Response:", data)
 	if data == null:
 		return
-
 	if "success" in data:
-
 		emit_signal("name_saved")
 
-	elif "text" in data:
 
-		emit_signal("quote_received",data)
+func _on_quote_completed(result, response_code, headers, body):
+	var response = body.get_string_from_utf8()
+	print("Quote response:", response)
+	var data = JSON.parse_string(response)
+	if data == null:
+		return
+	emit_signal("quote_received", data)
