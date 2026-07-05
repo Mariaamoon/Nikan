11a469d68e18177b3886e1bde160a13d3b3079e1
Maria <mariaamoon008@gmail.com>
2026-07-05 12:33:00 +0330

    training pairs

--- train2.py
+++ train2.py
@@ -1,5 +1,18 @@
 import pandas as pd
-df = pd.read_csv('C:/Users/jam/Documents/nikan/dataset/training.csv')
-print(df.head())
-print(df.columns)
-print(df.shape)
+
+
+train_df = pd.read_csv(r"C:\Users\jam\Documents\nikan\dataset\training.csv")
+val_df = pd.read_csv(r"C:\Users\jam\Documents\nikan\dataset\validation.csv")
+test_df = pd.read_csv(r"C:\Users\jam\Documents\nikan\dataset\test.csv")
+X_train = train_df["text"]
+y_train = train_df["label"]
+
+X_val = val_df["text"]
+y_val = val_df["label"]
+
+X_test = test_df["text"]
+y_test = test_df["label"]
+
+print(train_df.head())
+print(train_df.columns)
+print(train_df.shape)
