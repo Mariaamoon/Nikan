50245a6e9d92de8368c68dc3807b814e2b597709
Maria <mariaamoon008@gmail.com>
2026-07-05 21:10:38 +0330

    train

--- train_embedding_model.py
+++ train_embedding_model.py
@@ -1,11 +1,8 @@
 import pandas as pd
 
 df = pd.read_csv("dataset/training_pairs.csv")
-
 df = df[df["label"] == 1]
-
 df.to_csv("dataset/positive_pairs.csv", index=False)
-
 print(len(df))
 
 print("Unique pairs:", len(df.drop_duplicates()))
